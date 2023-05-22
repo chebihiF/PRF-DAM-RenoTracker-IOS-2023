f59aa081ff7f9d592154cf5a2527fba8df318570
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-22 08:36:19 +0100

    Initial Commit

---
+++ RenoTracker/RenoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenoTrackerApp.swift
+//  RenoTracker
+//
+//  Created by CHEBIHI FAYCAL on 22/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct RenoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
